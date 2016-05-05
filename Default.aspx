483e8baae2f6635b91afb9f3e1262348a587aae0
sudhips <sudhips21@gmail.com>
2016-05-06 00:18:08 +0530

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:RegDatabaseConnectionString %>" 
+            SelectCommand="SELECT * FROM [logintable]"></asp:SqlDataSource>
+    </h2>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            CellPadding="4" DataSourceID="SqlDataSource1" ForeColor="#333333" 
+            GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="id" HeaderText="id" SortExpression="id" />
+                <asp:BoundField DataField="username" HeaderText="username" 
+                    SortExpression="username" />
+                <asp:BoundField DataField="email" HeaderText="email" SortExpression="email" />
+                <asp:BoundField DataField="password" HeaderText="password" 
+                    SortExpression="password" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+</asp:Content>
---
+++ reg.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class reg : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegDatabaseConnectionString"].ConnectionString);
+            con.Open();
+            string user = "select count(*) from logintable where username='" + TextBox1.Text + "'";
+            SqlCommand com = new SqlCommand(user,con);
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+            if (temp == 1)
+            {
+                Response.Write("user exists already...!!!");
+            }
+
+            con.Close();
+        }
+    }
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Guid newguid = Guid.NewGuid();
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["RegDatabaseConnectionString"].ConnectionString);
+            con.Open();
+            string insert = "insert into logintable(id,username,email,password) values(@id,@uname,@email,@password)";
+            SqlCommand com = new SqlCommand(insert, con);
+            com.Parameters.AddWithValue("@id", newguid.ToString());
+            com.Parameters.AddWithValue("@uname", TextBox1.Text);
+            com.Parameters.AddWithValue("@email", TextBox2.Text);
+            com.Parameters.AddWithValue("@password", TextBox3.Text);
+            com.ExecuteNonQuery();
+            Response.Write("Registration Successful...!!");
+            Response.Redirect("default.aspx");
+            con.Close();
+        }
+        catch (Exception ex)
+        {
+            Response.Write("Error" + ex.ToString());
+        }
+    }
+}
